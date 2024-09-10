e04b5f344e42f1bde95d458fee61c9349a454eb0
Sakshi Shinde <sakshi.shinde@credoit.in>
2024-09-10 07:29:14 +0530

    commit the changes

--- Frontend/App/SkilledHandy/App.tsx
+++ Frontend/App/SkilledHandy/App.tsx
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View, TextInput, Button, Image } from "react-native";
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View>
+//         <Image source={require("./assets/AI.png")} style={styles.image} />
+//         <Text style={styles.title}>Illuminate the Path to</Text>
+//         <Text style={styles.title1}>Innovation with AI Mastery</Text>
+//       </View>
+//       <View style={{ marginBottom: "40%" }}>
+//         <Text style={[styles.email, styles.spacing]}>
+//           Enter Email ID to get OTP
+//         </Text>
+//       </View>
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//       <View style={[styles.inputBox, styles.spacing1]}>
+//         <TextInput
+//           style={{ flex: 1, color: "black" }}
+//           placeholder="Enter your Email Address"
+//           keyboardType={"numeric"}
+//         />
+//       </View>
+//       <View style={{ justifyContent: "flex-end" }}>
+//         <Button style={styles.button} title="Get OTP" />
+//       </View>
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   button: {
+//     margin: 50,
+//     overflow: "hidden",
+//     width: 350,
+//     borderRadius: 8,
+//   },
+//   inputContainer: {},
+//   inputBox: {
+//     borderColor: "black",
+//     borderWidth: 1,
+//     borderRadius: 10,
+//     height: "5%",
+//     width: "60%",
+//     flexDirection: "row",
+//     alignItems: "center",
+//     paddingLeft: 10,
+//     color: "black",
+//   },
+//   title: {
+//     fontSize: 30,
+//     alignItems: "center",
+//     color: "#2F88A0",
+//     fontWeight: "bold",
+//   },
+//   title1: {
+//     fontSize: 28,
+//     alignItems: "center",
+//     color: "#2F88A0",
+//     fontWeight: "bold",
+//   },
+//   email: {
+//     color: "#E7AA43",
+//     fontSize: 20,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   spacing: {
+//     // marginTop: 20,
+//   },
+//   spacing1: {
+//     // marginTop: 20,
+//   },
+//   image: {
+//     width: 250,
+//     backgroundColor: "black",
+//     height: "50%",
+//     borderRadius: 100,
+//     left: "10%",
+//   },
+// });
